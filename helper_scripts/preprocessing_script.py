80d8d959946cfbe9cc2be529058db1ed16bf7601
dsgorthy <derek.gorthy@colorado.edu>
2017-04-06 21:34:02 -0600

    Wrote and tested function to get list of days depending on the month

--- helper_scripts/preprocessing_script.py
+++ helper_scripts/preprocessing_script.py
@@ -10,12 +10,38 @@ import csv
 import os
 
 
-
 def add_line_to_file(line,output_file_name):
 	with open(output_file_name, "a") as f:
 		f.write(line)
 	return
 
+
+# Specific to 2014, can be made more extensible if needed
+#
+# Takes day_of_week: Int (Monday = 1, Tuesday = 2, ...)
+#
+def get_list_of_possible_dates(day_of_week, month):
+	
+	# Day of week on 1st, Number of days
+	# Monday = 1
+	date_data = [[3,31],[6,28],[6,31],[2,30],[4,31],[7,30],
+					[2,31],[5,31],[1,30],[3,31],[6,30],[1,31]]
+
+	possible_days = []
+	cur_month = date_data[month-1]
+	first_day = cur_month[0]
+
+	if (day_of_week == first_day):
+		possible_days.append(1)
+	elif (day_of_week > first_day):
+		possible_days.append(1+day_of_week-first_day)
+	else:
+		possible_days.append(8-(first_day-day_of_week))
+
+	for date in range(possible_days[0]+7,cur_month[1],7):
+		possible_days.append(date)
+
+	return possible_days
 
 
 def remove_column(file_name, column_list, new_file_name):
@@ -52,7 +78,13 @@ def remove_column(file_name, column_list, new_file_name):
 	return
 
 
+def estimate_dates_from_day_of_week_uniform(input_file, output_file_name):
 
-remove_list = ['CurrentDataYear', 'RaceRecode3']
+	return
 
-remove_column("../data/DeathRecords.csv", remove_list, "../data/DeathRecords_ver2.csv")
+
+
+
+# Used to remove redundant columns of data, can be used again if more found redundant
+#remove_list = ['CurrentDataYear', 'RaceRecode3']
+#remove_column("../data/DeathRecords.csv", remove_list, "../data/DeathRecords_ver2.csv")
