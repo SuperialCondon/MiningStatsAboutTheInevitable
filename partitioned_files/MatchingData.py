8412ab76c6fd8f91879a217d20a7ed16d76c12a1
Bryce Melvin <Bryce_Melvin@engr2-2-93-161-dhcp.int.colorado.edu>
2017-04-25 17:18:25 -0600

    Updated function to allow for any number of codes

--- partitioned_files/MatchingData.py
+++ partitioned_files/MatchingData.py
@@ -12,13 +12,15 @@ def calculateCSV():
     #userFile = raw_input("Please enter a file name: ")
     #fileUse = userFile + ".csv"
     with open(fileUse,'r') as filename:
-        userCode = raw_input("Please enter a Icd10Code: ")
+        userCode = raw_input("Please enter a Icd10Code(s) seperated by commas: ")
         actualUserCode = userCode.upper()
+        listOfCodes = actualUserCode.split(',')
         counter = 0
         for line in filename:
-            lineList = line.split(',') #Loop through file to find the corresponding death code and count how many appear in dataset
-            if(lineList[24] == actualUserCode):
-                counter+= 1
+            lineList = line.split(',') #Loop through file to find the corresponding death code(s) and count how many appear in dataset
+            for code in listOfCodes:
+                if(lineList[24] == code):
+                    counter+= 1
     return counter
 
 print calculateCSV()
