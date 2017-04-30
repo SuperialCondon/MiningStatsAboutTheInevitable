dfc93ca0f5ff879f685993753a7c545b925be24e
Bryce Melvin <Bryce_Melvin@res1-156-174-dhcp.int.colorado.edu>
2017-04-30 14:43:06 -0600

    Updated validation script

--- post_processing/ValidationScriptR.R
+++ post_processing/ValidationScriptR.R
@@ -1,17 +1,15 @@
 #R Script to validate CDC's findings.
 #Derric Gorothy, Michael Condon, Bryce Melvin  
-
-data = read.csv(file = "DeathRecords_pt0.csv", header = TRUE)
+userFile = readline(prompt = "Please enter the csv file to be used: ")
+data = read.csv(file = userFile, header = TRUE)
 counter = 0
 icd10code = data[,25]
 list = NULL
 code = readline(prompt= "Please enter a code (in all caps): ")
-for(i in icd10code)
-  {
+
+for(i in icd10code) {
   list = c(list, i)
-  if(i == code)
-    {
+  if(i == code){
     counter = counter + 1
   }
 }
-print(counter)
