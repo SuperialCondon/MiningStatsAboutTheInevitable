200aefed65c32730f1cc361c98f4ff6fb6f5a133
dsgorthy <derek.gorthy@colorado.edu>
2017-05-01 01:15:09 -0600

    Updated the simple counting scripts

--- post_processing/counter.py
+++ post_processing/counter.py
@@ -19,6 +19,9 @@ def add_line_to_file(init_str,output_file_name):
 	return
 
 
+# Takes dictionary and writes to a file
+# Output file will not be ordered!
+#
 def dict_to_file(output_file, this_dict):
 
 	od = collections.OrderedDict(sorted(this_dict.items()))
@@ -34,10 +37,14 @@ def dict_to_file(output_file, this_dict):
 	return
 
 
+# Simple counting method, requires the key and value indices, also requires a list of possibilities
+# Allows user to ignore certain possibilities to ensure the output files are not bloated
+#
 def count_variations(input_directory_path, output_file_path, key_index, value_index, first_line, header_string, relevant_possibilities):
 
 	temp_dict = {}
 
+	# Loop through partioned files directory
 	for filename in os.listdir(input_directory_path):
 		print("Reading in file "+filename)
 		f = open(input_directory_path+filename)
@@ -48,11 +55,13 @@ def count_variations(input_directory_path, output_file_path, key_index, value_in
 			if(first_line):
 				first_line = False
 			else:
+				# Check if entry exists
 				if row[key_index] not in temp_dict:
 					this_value = []
 					for i in range(0,len(relevant_possibilities)):
 						this_value.append(0)
 					temp_dict[row[key_index]] = this_value
+				# Only loop through relevant indices
 				for i in range(0,len(relevant_possibilities)):
 					if (relevant_possibilities[i] == row[value_index]):
 						temp_dict[row[key_index]][i] += 1
@@ -63,6 +72,9 @@ def count_variations(input_directory_path, output_file_path, key_index, value_in
 				
 	return
 
-count_variations("../partitioned_files/", "../totals/total_deaths_by_AgeRecord12.csv", 13, 19, 1, \
-	"AgeRecode12, Not Specified, Accident, Suicide, Homicide, Pending Investigation, Could Not be Determined, Self-inflicted Natural", \
-	 ["0","1","2","3","4","5","6","7"])
+
+# EXAMPLE USAGE
+#
+#count_variations("../partitioned_files/", "../totals/total_deaths_by_AgeRecord12.csv", 13, 19, 1, \
+#	"AgeRecode12, Not Specified, Accident, Suicide, Homicide, Pending Investigation, Could Not be Determined, Self-inflicted Natural", \
+#	 ["0","1","2","3","4","5","6","7"])
